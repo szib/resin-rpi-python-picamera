0d284e4923a6c024884cd8ebe7851812caaf7daa
szib <szib@users.noreply.github.com>
2017-03-11 14:39:16 +0000

    Add Brett's device hostname

--- client.py
+++ client.py
@@ -7,7 +7,8 @@ import picamera
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
 client_socket = socket.socket()
-client_socket.connect(('my_server', 8000))
+# client_socket.connect(('238eefd7d00ebd96f0f2ef195c7e05fd.resindevice.io', 8000))
+client_socket.connect(('52.4.252.97', 8000))
 
 # Make a file-like object out of the connection
 connection = client_socket.makefile('wb')
